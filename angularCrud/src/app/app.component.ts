92ed8c43a78d39bdd89da2f7eb53f3376286e892
Nelson Alvaro <n.morante@empresasim.com>
2021-08-09 13:54:15 -0400

    creando metodos de eliminar acutlizar y guardar con mensaje de alers

--- angularCrud/src/app/app.component.ts
+++ angularCrud/src/app/app.component.ts
@@ -7,22 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'angular Crud';
+  msg: string = '';
 
   employees = [
-    { 'name': 'Pedro', position: 'manager' },
-    { 'name': 'Marco', position: 'designer' },
-    { 'name': 'Ever', position: 'programmer' }
+    { 'name': 'Pedro', position: 'manager', email: 'asd@gmail.com' },
+    { 'name': 'Marco', position: 'designer', email: 'asd@gmail.com' },
+    { 'name': 'Ever', position: 'programmer', email: 'asd@gmail.com' }
   ];
 
   model: any = {};
+  model2: any = {};
+  hideUpdate: boolean = true;
 
   addEmpleyee(): void {
+    this.employees.push(this.model);
+    this.msg = 'campo agregado';
+  }
+
+  deleteEmployee(i: number): void {
+    var answer = confirm('¿seguro que desea eliminarlo?');
+    if (answer) {
+      this.employees.splice(i, 1);
+      this.msg = 'campo eliminado';
+    }
 
   }
 
-  deleteEmployee(): void { }
+  myValue: number = 0;
+  editEmployee(i: number): void {
+    this.hideUpdate = false;
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
+  }
 
-  editEmployee(): void { }
+  updateEmployee(): void {
+    let i = this.myValue;
+    for (let j = 0; j < this.employees.length; j++) {
+      if (i == j) {
+        this.employees[i] = this.model2;
+        this.msg = 'campo actualziado';
+        this.model2 = {};
+      }
+    }
 
-  updateEmployee(): void { }
+  }
+
+  closeAlert(): void {
+    this.msg = '';
+  }
 }
